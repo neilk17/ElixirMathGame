5003479334b3185f11638e32cd507d9ede3363ba
Neil Kanakia <neil@MacBook-Pro.local>
2022-05-14 14:29:17 -0400

    generate multiplication problems

--- Assets/Scripts/random.cs
+++ Assets/Scripts/random.cs
@@ -17,16 +17,15 @@ namespace RandomProblem
         {
             List<string> singProb = new List<string>();
             string operatorString;
-            int const2, const4;
+            int const1, const2, const3, const4;
 
 
             // Selecting random answer, constants and operator
             Random rnd = new Random();
-            int ans = rnd.Next(1, 30);
-            int const1 = rnd.Next(1, ans);
-            int const3 = rnd.Next(1, ans);
-            int const5 = rnd.Next(const1, ans);
-            var operation = rnd.Next(1, 3);  
+            int ans = rnd.Next(2, 30);
+            int const5 = rnd.Next(1, ans);
+            var operation = rnd.Next(1, 4);  
+            // var operation = 3;
 
             singProb.Add(ans.ToString());
 
@@ -35,14 +34,34 @@ namespace RandomProblem
                 case 1:
                     operatorString = "+";
                     singProb.Add(operatorString);
+                    const1 = rnd.Next(1, ans);
+                    const3 = rnd.Next(1, ans);
                     const2 = ans - const1;
                     const4 = ans - const3;
                     break;
                 case 2:
                     operatorString = "-";
                     singProb.Add(operatorString);
+                    const1 = rnd.Next(1, ans);
+                    const3 = rnd.Next(1, ans);
                     const2 = ans + const1;
                     const4 = ans + const3;
+                    break;
+                case 3:
+                    operatorString = "*";
+                    singProb.Add(operatorString);
+                    do {
+                        const1 = rnd.Next(1, ans);
+                    } while (ans % const1 != 0);
+                    const2 = ans / const1;
+                    do {
+                        const3 = rnd.Next(1, ans);
+                        if (const3 == const1 || const3 == const2) 
+                        {
+                            break;
+                        }
+                    } while (ans % const3 != 0);
+                    const4 = ans / const3;
                     break;
                 default:
                     operatorString = "?";
